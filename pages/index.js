1e89bf0ea49ac1da4ee4a78dd1ea191238a00729
Mehdi BHA <mehdibenhadjali@gmail.com>
2022-07-09 00:08:55 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Mehdi BHA
         </h1>
 
         <p className={styles.description}>

8b55501032928f20395c61b0920a1aac0a6b8c2b
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-21 20:07:05 +0100

    added script to generate sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  generateIndexSitemap: false
+  // ...other options
+  // https://github.com/iamvishnusankar/next-sitemap#configuration-options
+}

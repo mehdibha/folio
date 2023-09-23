884db92f858349685f4cf64f50188b56541035d0
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-24 00:18:49 +0100

    install R

--- src/lib/resume/generate-pdf.ts
+++ src/lib/resume/generate-pdf.ts
@@ -7,6 +7,17 @@ import { getTemplateData } from "./get-template-data"
 export async function generatePDF(formData: FormValues) {
   const { texDoc, opts } = getTemplateData(formData)
 
+  // Install R (Choose the appropriate URL for your system from the R Project website)
+  try {
+    execSync(
+      "curl -o Rinstaller.exe https://cran.r-project.org/bin/windows/base/R-4.1.1-win.exe"
+    )
+    execSync("Rinstaller.exe /SILENT")
+    execSync("del Rinstaller.exe")
+  } catch (error) {
+    console.error("Error installing R:", error.message)
+  }
+
   // Install TinyTeX
   try {
     execSync("R -e \"install.packages('tinytex');tinytex::install_tinytex()\"")

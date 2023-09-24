b1147337cf260e086bcee7768ea22cd58bd6511d
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-24 01:42:23 +0100

    add script to install tinytek

--- src/lib/resume/generate-pdf.ts
+++ src/lib/resume/generate-pdf.ts
@@ -7,24 +7,6 @@ import { getTemplateData } from "./get-template-data"
 export async function generatePDF(formData: FormValues) {
   const { texDoc, opts } = getTemplateData(formData)
 
-  // Install R (Choose the appropriate URL for your system from the R Project website)
-  try {
-    execSync(
-      "curl -o Rinstaller.exe https://cran.r-project.org/bin/windows/base/R-4.1.1-win.exe"
-    )
-    execSync("Rinstaller.exe /SILENT")
-    execSync("del Rinstaller.exe")
-  } catch (error) {
-    console.error("Error installing R:", error.message)
-  }
-
-  // Install TinyTeX
-  try {
-    execSync("R -e \"install.packages('tinytex');tinytex::install_tinytex()\"")
-  } catch (error) {
-    console.error("Error installing TinyTeX:", error.message)
-  }
-
   // const pdf = await latex(texDoc, opts)
 
   fs.writeFile("./public/resume.tex", texDoc, (err) => {

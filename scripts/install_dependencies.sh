18f8d060d00e3f3bda47ed67d86cfd908c986218
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-24 02:49:08 +0100

    tes

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,8 +5,14 @@ yum install wget
 # Define the installation directory within your project
 INSTALL_DIR="$HOME/project/tinytex"
 
+# Add TinyTeX to the system PATH
+# "$INSTALL_DIR/bin/*/tlmgr" path add
 # Download and install TinyTeX
 curl -sL "https://yihui.org/tinytex/install-bin-unix.sh" | sh -s - --admin --dir "$INSTALL_DIR"
 
 # Add TinyTeX to the system PATH
-# "$INSTALL_DIR/bin/*/tlmgr" path add
+echo "export PATH=\"$INSTALL_DIR/bin/x86_64-linux:\$PATH\"" >> ~/.bashrc
+source ~/.bashrc  # Refresh the shell environment
+
+# Print the TinyTeX version
+"$INSTALL_DIR/bin/x86_64-linux/tlmgr" --version
--- src/lib/resume/generate-pdf.ts
+++ src/lib/resume/generate-pdf.ts
@@ -11,11 +11,11 @@ export async function generatePDF(formData: FormValues) {
   // Import the child_process module
 
   // Install TinyTeX
-  try {
-    execSync('curl -sL "https://yihui.org/tinytex/install-bin-unix.sh" | sh')
-  } catch (error) {
-    console.error("Error installing TinyTeX:", error)
-  }
+  // try {
+  //   execSync('curl -sL "https://yihui.org/tinytex/install-bin-unix.sh" | sh')
+  // } catch (error) {
+  //   console.error("Error installing TinyTeX:", error)
+  // }
 
   fs.writeFile("./public/resume.tex", texDoc, (err) => {
     if (err) {

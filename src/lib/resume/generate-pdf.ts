ee7a441c6cd0642b5ad638d4638b6271c67517e7
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-24 03:45:00 +0100

    test

--- src/lib/resume/generate-pdf.ts
+++ src/lib/resume/generate-pdf.ts
@@ -17,7 +17,72 @@ export async function generatePDF(formData: FormValues) {
   //   console.error("Error installing TinyTeX:", error)
   // }
 
-  fs.writeFile("./public/resume.tex", texDoc, (err) => {
+  const content = `\documentclass{article} % Document class (article, report, book, etc.)
+    % Dummy text generator
+  
+  % Title and Author
+  \title{Your Title Here}
+  \author{Your Name}
+  \date{\today} % Use \date{} to set a specific date
+  
+  \begin{document}
+  
+  \maketitle % Generates the title based on the information provided above
+  
+  \section{Introduction}
+  
+  This is the introduction section of your document. You can start writing your content here.
+  
+  \section{Main Content}
+  
+  Here's some more content. You can use various LaTeX commands for formatting, such as \textit{italic}, \textbf{bold}, and \texttt{typewriter} text. You can also create lists:
+  
+  \begin{itemize}
+    \item Item 1
+    \item Item 2
+    \item Item 3
+  \end{itemize}
+  
+  Or numbered lists:
+  
+  \begin{enumerate}
+    \item First item
+    \item Second item
+    \item Third item
+  \end{enumerate}
+  
+  You can include figures:
+  
+  \begin{figure}[h]
+    \centering
+    \includegraphics[width=0.5\textwidth]{example-image}
+    \caption{A sample figure.}
+    \label{fig:sample}
+  \end{figure}
+  
+  And you can reference figures like this: Figure~\ref{fig:sample}.
+  
+  \subsection{Subsection}
+  
+  You can create subsections to organize your content further.
+  
+  \section{Conclusion}
+  
+  This is the conclusion section of your document.
+  
+  \section*{Acknowledgments} % Unnumbered section
+  
+  You can also create unnumbered sections like acknowledgments.
+  
+  \appendix % Start appendices
+  
+  \section{Appendix A}
+  
+  You can include appendices in your document as well.
+  
+  \end{document}`
+
+  fs.writeFile("./public/resume.tex", content, (err) => {
     if (err) {
       console.error("Error writing to output.tex:", err)
     } else {

b1d072938dfd4af8a7a6a65a10df639dde3782e4
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-24 02:10:55 +0100

    ins

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "Hello world"
+# Define the installation directory within your project
+INSTALL_DIR="$HOME/project/tinytex"
+
+# Download and install TinyTeX
+curl -sL "https://yihui.org/tinytex/install-bin-unix.sh" | sh -s - --admin --dir "$INSTALL_DIR"
+
+# Add TinyTeX to the system PATH
+"$INSTALL_DIR/bin/*/tlmgr" path add

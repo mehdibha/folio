fd053f7edc58bdb9f647ee517b6fc23d9b098b1f
Mehdi BHA <mehdibenhadjali@gmail.com>
2023-09-24 03:29:18 +0100

    last try

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-# Install curl and perl-Digest-MD5 if they're not already installed
-if ! command -v curl &> /dev/null; then
-   yum install curl -y
-fi
+# Update the package manager and install TeX Live
+yum update -y
+yum install texlive texlive-latex -y
 
-if ! perl -MDigest::MD5 -e 1 &> /dev/null; then
-   yum install perl-Digest-MD5 -y
-fi
+# Print TeX Live version
+echo "pdflatex --version"
+pdflatex --version
 
-# Define the installation directory for TinyTeX
-INSTALL_DIR="$HOME/.TinyTeX"
-
-# Check if TinyTeX is already installed
-if [ ! -d "$INSTALL_DIR" ]; then
-    # Download and install TinyTeX
-    curl -sL "https://yihui.org/tinytex/install-bin-unix.sh" | sh -s - --admin --dir "$INSTALL_DIR"
-fi
-
-# Add TinyTeX to the system PATH
-echo "export PATH=\"$HOME/.TinyTeX/bin/x86_64-linux:\$PATH\"" >> ~/.bashrc
-source ~/.bashrc  # Refresh the shell environment
-
-# Print the TinyTeX version
-"$HOME/.TinyTeX/bin/x86_64-linux/tlmgr" --version
+# Optionally, install additional TeX Live packages as needed
+# sudo yum install texlive-<package-name> -y
